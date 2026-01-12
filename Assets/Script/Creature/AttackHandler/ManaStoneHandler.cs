97974d04ae8794f8af162068e65ba7669c7cc944
doveHa <hadove02@gmail.com>
2026-01-12 16:26:32 +0900

    Summoner Debugging

---
+++ Assets/Script/Creature/AttackHandler/ManaStoneHandler.cs
@@ -0,0 +1,58 @@
+﻿using Script.BattleStyle.Manager;
+using UnityEngine;
+
+namespace Script.Creature.AttackHandler
+{
+    public class ManaStoneHandler : MonoBehaviour
+    {
+        public int bonusCostAmount = 25;
+        public float productionTime = 5.0f;
+
+        public GameObject readyIcon;
+
+        
+        private float currentTimer;
+        private bool isReady = false;
+
+        private void Start()
+        {
+            currentTimer = 0f;
+        }
+
+        private void Update()
+        {
+            if (isReady) return;
+
+            currentTimer += Time.deltaTime;
+
+            if (currentTimer >= productionTime)
+            {
+                SetReadyState(true);
+            }
+        }
+
+        private void SetReadyState(bool ready)
+        {
+            isReady = ready;
+
+            if (readyIcon != null)
+                readyIcon.SetActive(ready);
+        }
+
+        private void OnMouseDown()
+        {
+            if (isReady)
+            {
+                CollectResource();
+            }
+        }
+
+        // 코스트 획득 로직
+        private void CollectResource()
+        {
+            CostManager.Manager.AddCost(bonusCostAmount);
+            currentTimer = 0f;
+            SetReadyState(false);
+        }
+    }
+}
